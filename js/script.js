82b481448070b20e57543fc79b3468ea5b757287
nicolocamassa <camassanicolo03@gmail.com>
2023-12-01 16:48:44 +0100

    [UPDATE] number disappear after timer

--- js/script.js
+++ js/script.js
@@ -8,20 +8,31 @@ setInterval(function(){
 },1000) */
 
 function randomNumberSimon() {
+    /* Vettore dei numeri casuali di Simon */
     let simonNumbers = [];
+
+    /* Inizializzazione del tag ul */
     let numList = document.getElementById('num-list');
+    let li = document.createElement('li');
+    
+        /* Generazione dei 5 numeri */
+        for (let i = 0; i <= 5; i++) {
 
-    for(let i = 0; i <= 5; i++){
-        
-        let li = document.createElement('li');
-        
+            let li = document.createElement('li');
+            let randomNumber = Math.floor(Math.random() * 100) + 1;
+            /* Aggiunta dei numeri in un vettore */
+            simonNumbers.push(randomNumber);
 
-        let randomNumber = Math.floor(Math.random() * 100 ) + 1;
-        simonNumbers.push(randomNumber);
+            /* Stampa dei numeri in index */
+            li.innerText = simonNumbers[i];
+            numList.appendChild(li);
+        }
 
-        li.innerText = simonNumbers[i];
-        numList.appendChild(li);
-    }
+    setTimeout(function(){
+        numList.innerText = '';
+    },30000)
+    
+   
 }
 
 randomNumberSimon();
